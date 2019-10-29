26c5a5666bf1a2b81dcb201b1b9eac8a94133da7
Javier Mora <jinjuriki62@gmail.com>
2019-10-29 14:23:46 -0600

    Mas o menos

--- app/src/main/java/com/morajavier/raza/MainActivity.java
+++ app/src/main/java/com/morajavier/raza/MainActivity.java
@@ -7,15 +7,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
     Button jBtnRaza;
+    ImageView imgPresentacion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         jBtnRaza = findViewById(R.id.xbtnListImage);
+        imgPresentacion = findViewById(R.id.imageViewPresent);
+        imgPresentacion.setImageResource(R.drawable.mask);
         jBtnRaza.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
